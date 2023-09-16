634afae36cbfba4637db9bd55c1092f1051c5ceb
ni3 <nik.nikn.nik@gmail.com>
2023-09-16 13:02:06 +0530

    implemented swagger

--- task_management_system/task_management_system/settings.py
+++ task_management_system/task_management_system/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'task',
     'users',
+    'drf_yasg',
 ]
 
 REST_FRAMEWORK = {
@@ -150,3 +151,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 
+SWAGGER_SETTINGS = {
+    'USE_SESSION_AUTH': False,  # Set to True if you want to use session authentication
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header',
+        },
+    },
+}
